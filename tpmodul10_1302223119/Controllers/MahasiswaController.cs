47649a0a5cff790a701dd709be8a076932dcd2da
safwandm <safwan.damanik@gmail.com>
2024-04-28 08:35:06 +0700

    first commit

---
+++ tpmodul10_1302223119/Controllers/MahasiswaController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace tpmodul10_1302223119.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MahasiswaController : ControllerBase
+    {
+        private string[] Siswa = new[]
+        {
+            "siswa1", "siswa2", "siswa3"
+        };
+
+        [HttpGet]
+        public IEnumerable<string> GetSiswa()
+        {
+            return Siswa;
+        }
+    }
+}
